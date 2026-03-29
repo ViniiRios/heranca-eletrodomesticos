3032b6f1b5a510f5a268789fc3e0276057c594bc
ViniiRios <viniciusraphael8@gmail.com>
2026-03-29 02:59:02 -0300

    feat: adiciona estrutura inicial da superclasse Eletrodomestico

--- src/eletrodomesticos/Eletrodomestico.java
+++ src/eletrodomesticos/Eletrodomestico.java
@@ -1,5 +1,22 @@
 package eletrodomesticos;
 
 public class Eletrodomestico {
+    private String marca;
+    private String modelo;
+    private int voltagem;
+    private boolean ligado;
+
+    // Construtor vazio
+    public Eletrodomestico() {
+    }
+
+    // Construtor com dados
+    public Eletrodomestico(String marca, String modelo, int voltagem) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.voltagem = voltagem;
+        this.ligado = false; // começa desligado
+    }
+
     
 }
